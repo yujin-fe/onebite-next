1a196717cdededb48bfe6ef61f4996a16279ed18
yujin-fe <hyujin1224@gmail.com>
2025-11-03 13:54:33 +0900

    chore:배포주로로 백엔드 주소 변경

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -1,7 +1,7 @@
 import {BookData} from "../types"
 
 export default async function fetchOneBook (id:number):Promise<BookData|null>{
-  const url = `http://localhost:12345/book/${id}`
+  const url = `https://onebite-books-server-main-sigma.vercel.app/${id}`
 
   try{
     const response = await fetch(url);
