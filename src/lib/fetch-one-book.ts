9f778ab82561f91e3259ada2cd5f46f3338f0f6b
yujin-fe <hyujin1224@gmail.com>
2025-11-03 13:59:13 +0900

    fix:백엔드 주소 오타 수정

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -1,7 +1,7 @@
 import {BookData} from "../types"
 
 export default async function fetchOneBook (id:number):Promise<BookData|null>{
-  const url = `https://onebite-books-server-main-sigma.vercel.app/${id}`
+  const url = `https://onebite-books-server-main-sigma.vercel.app/book/${id}`
 
   try{
     const response = await fetch(url);
